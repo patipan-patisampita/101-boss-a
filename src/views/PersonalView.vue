0a58c2f6e21929b83c00ae78888ab006250b6896
patipan-patisampita <patipan.patisampita@gmail.com>
2024-01-30 02:38:34 +0700

    update

---
+++ src/views/PersonalView.vue
@@ -0,0 +1,300 @@
+<template>
+    <div class="container my-4">
+        <div class="row">
+
+            <div class="col-md-3">
+                <div class="card shadow p-2">
+                    <div class="card-header p-0">
+                        <img src="../assets/images/vichakan.jpg" class="card-img-top" alt="img">
+                    </div>
+                    <div class="card-body text-center">
+                        <span class="card-title" style="color: #FF8C00; font-size: 18px;">นายยงยุทธ พนัสนอก</span>
+                        <p class="card-text" style="color: #006400; font-size: 17px;">รองผู้อำนวยการ ฝ่ายวิชาการ</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-9">
+                <div class="card my-2">
+                    <div class="card-header p-2">
+                        <p class="card-text text-bold" style="color: #008B8B; font-size: 17px;"><i
+                                class="fa-brands fa-leanpub me-1"></i>งานในฝ่ายวิชาการ</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="table-responsive">
+                        <table class="table align-items-center mb-0">
+                            <thead>
+                                <tr>
+                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Author
+                                    </th>
+                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                        Function</th>
+                                    <th
+                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Technology</th>
+                                    <th
+                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Employed</th>
+                                    <th class="text-secondary opacity-7"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <div class="d-flex px-2 py-1">
+                                            <div>
+                                                <a href="https://rms.trattc.ac.th/pms_graph.php?p=&people_dep_id=325&mod=&title=%A7%D2%B9%C7%D1%B4%BC%C5%E1%C5%D0%BB%C3%D0%E0%C1%D4%B9%BC%C5#data"
+                                                    target="_blank" class="text-secondary font-weight-normal text-xs"
+                                                    data-toggle="tooltip" data-original-title="Edit user">
+                                                    <img src="https://rms.trattc.ac.th/files/56282_22081710102923.jpg"
+                                                        class="avatar avatar-sm me-3">
+                                                </a>
+
+                                            </div>
+                                            <div class="d-flex flex-column justify-content-center">
+                                                <h6 class="mb-0 text-xs">นายมงคลศักดิ์ ดวงพุทธา</h6>
+                                                <p class="text-xs text-secondary mb-0">john@creative-tim.com</p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <p class="text-xs font-weight-bold mb-0">หัวหน้างาน</p>
+                                        <p class="text-xs text-secondary mb-0">งานวัดผลและประเมินผล</p>
+                                    </td>
+                                    <td class="align-middle text-center text-sm">
+                                        <span class="badge badge-sm badge-success">Online</span>
+                                    </td>
+                                    <td class="align-middle text-center">
+                                        <span class="text-secondary text-xs font-weight-normal">ครูผู้ช่วย</span>
+                                    </td>
+                                    <td class="">
+                                        <a href="https://rms.trattc.ac.th/pms_graph.php?p=&people_dep_id=325&mod=&title=%A7%D2%B9%C7%D1%B4%BC%C5%E1%C5%D0%BB%C3%D0%E0%C1%D4%B9%BC%C5#data"
+                                            target="_blank" class="text-secondary font-weight-normal text-xs"
+                                            data-toggle="tooltip" data-original-title="Edit user">
+                                            Details
+                                        </a>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td>
+                                        <div class="d-flex px-2 py-1">
+                                            <div>
+                                                <a href="https://rms.trattc.ac.th/pms_graph.php?p=&people_dep_id=322&mod=&title=%A7%D2%B9%BE%D1%B2%B9%D2%CB%C5%D1%A1%CA%D9%B5%C3%A1%D2%C3%E0%C3%D5%C2%B9%A1%D2%C3%CA%CD%B9#data"
+                                                    target="_blank" class="text-secondary font-weight-normal text-xs"
+                                                    data-toggle="tooltip" data-original-title="Edit user">
+                                                    <img src="https://rms.trattc.ac.th/files/74981_21052111111646.jpg"
+                                                        class="avatar avatar-sm me-3">
+                                                </a>
+
+                                            </div>
+                                            <div class="d-flex flex-column justify-content-center">
+                                                <h6 class="mb-0 text-xs">นายชาญวิทย์ โทมา</h6>
+                                                <p class="text-xs text-secondary mb-0">alexa@creative-tim.com</p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <p class="text-xs font-weight-bold mb-0">หัวหน้างาน</p>
+                                        <p class="text-xs text-secondary mb-0">งานพัฒนาหลักสูตรการเรียนการสอน</p>
+                                    </td>
+                                    <td class="align-middle text-center text-sm">
+                                        <span class="badge badge-sm badge-secondary">Offline</span>
+                                    </td>
+                                    <td class="align-middle text-center">
+                                        <span class="text-secondary text-xs font-weight-normal">ครูผู้ช่วย</span>
+                                    </td>
+                                    <td class="align-middle">
+                                        <a href="https://rms.trattc.ac.th/pms_graph.php?p=&people_dep_id=322&mod=&title=%A7%D2%B9%BE%D1%B2%B9%D2%CB%C5%D1%A1%CA%D9%B5%C3%A1%D2%C3%E0%C3%D5%C2%B9%A1%D2%C3%CA%CD%B9#data"
+                                            target="_blank" class="text-secondary font-weight-normal text-xs"
+                                            data-toggle="tooltip" data-original-title="Edit user">
+                                            Details
+                                        </a>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td>
+                                        <div class="d-flex px-2 py-1">
+                                            <div>
+
+                                                <a href="https://rms.trattc.ac.th/pms_graph.php?p=&people_dep_id=326&mod=&title=%A7%D2%B9%CA%D7%E8%CD%A1%D2%C3%E0%C3%D5%C2%B9%A1%D2%C3%CA%CD%B9#data"
+                                                    target="_blank" class="text-secondary font-weight-normal text-xs"
+                                                    data-toggle="tooltip" data-original-title="Edit user">
+                                                    <img src="https://rms.trattc.ac.th/files/57767_21080114145526.jpg"
+                                                        class="avatar avatar-sm me-3">
+                                                </a>
+                                            </div>
+                                            <div class="d-flex flex-column justify-content-center">
+                                                <h6 class="mb-0 text-xs">นายธนิต เนืองเฉลิม</h6>
+                                                <p class="text-xs text-secondary mb-0">laurent@creative-tim.com</p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <p class="text-xs font-weight-bold mb-0">หัวหน้างาน</p>
+                                        <p class="text-xs text-secondary mb-0">งานสื่อการเรียนการสอน</p>
+                                    </td>
+                                    <td class="align-middle text-center text-sm">
+                                        <span class="badge badge-sm badge-success">Online</span>
+                                    </td>
+                                    <td class="align-middle text-center">
+                                        <span class="text-secondary text-xs font-weight-normal">ครูผู้ช่วย</span>
+                                    </td>
+                                    <td class="align-middle">
+                                        <a href="https://rms.trattc.ac.th/pms_graph.php?p=&people_dep_id=326&mod=&title=%A7%D2%B9%CA%D7%E8%CD%A1%D2%C3%E0%C3%D5%C2%B9%A1%D2%C3%CA%CD%B9#data"
+                                            target="_blank" class="text-secondary font-weight-normal text-xs"
+                                            data-toggle="tooltip" data-original-title="Edit user">
+                                            Details
+                                        </a>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td>
+                                        <div class="d-flex px-2 py-1">
+                                            <div>
+                                                <a href="https://rms.trattc.ac.th/pms_graph.php?p=&people_dep_id=308&mod=&title=%A7%D2%B9%C7%D4%B7%C2%BA%C3%D4%A1%D2%C3%E1%C5%D0%CB%E9%CD%A7%CA%C1%D8%B4(%CB%E9%CD%A7%CA%C1%D8%B4)#data"
+                                                    target="_blank" class="text-secondary font-weight-normal text-xs"
+                                                    data-toggle="tooltip" data-original-title="Edit user">
+                                                    <img src="https://rms.trattc.ac.th/files/55182_23062012124626.jpg"
+                                                        class="avatar avatar-sm me-3">
+                                                </a>
+
+                                            </div>
+                                            <div class="d-flex flex-column justify-content-center">
+                                                <h6 class="mb-0 text-xs">นางสาวปิยะธิดา พัฒนราช</h6>
+                                                <p class="text-xs text-secondary mb-0">michael@creative-tim.com</p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <p class="text-xs font-weight-bold mb-0">หัวหน้างาน</p>
+                                        <p class="text-xs text-secondary mb-0">งานวิทยบริการและห้องสมุด(ห้องสมุด)</p>
+                                    </td>
+                                    <td class="align-middle text-center text-sm">
+                                        <span class="badge badge-sm badge-success">Online</span>
+                                    </td>
+                                    <td class="align-middle text-center">
+                                        <span class="text-secondary text-xs font-weight-normal">ครูผู้ช่วย</span>
+                                    </td>
+                                    <td class="align-middle">
+                                        <a href="https://rms.trattc.ac.th/pms_graph.php?p=&people_dep_id=308&mod=&title=%A7%D2%B9%C7%D4%B7%C2%BA%C3%D4%A1%D2%C3%E1%C5%D0%CB%E9%CD%A7%CA%C1%D8%B4(%CB%E9%CD%A7%CA%C1%D8%B4)#data"
+                                            target="_blank" class="text-secondary font-weight-normal text-xs"
+                                            data-toggle="tooltip" data-original-title="Edit user">
+                                            Details
+                                        </a>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td>
+                                        <div class="d-flex px-2 py-1">
+                                            <div>
+                                                <a href="https://rms.trattc.ac.th/pms_graph.php?p=&people_dep_id=327&mod=&title=%A7%D2%B9%CD%D2%AA%D5%C7%C8%D6%A1%C9%D2%C3%D0%BA%BA%B7%C7%D4%C0%D2%A4%D5#data"
+                                                    target="_blank" class="text-secondary font-weight-normal text-xs"
+                                                    data-toggle="tooltip" data-original-title="Edit user">
+                                                    <img src="https://rms.trattc.ac.th/files/31269_23081016161704.jpg"
+                                                        class="avatar avatar-sm me-3">
+                                                </a>
+
+                                            </div>
+                                            <div class="d-flex flex-column justify-content-center">
+                                                <h6 class="mb-0 text-xs">นายชมภูศักดิ์ พนากอบกิจ</h6>
+                                                <p class="text-xs text-secondary mb-0">richard@creative-tim.com</p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <p class="text-xs font-weight-bold mb-0">หัวหน้างาน</p>
+                                        <p class="text-xs text-secondary mb-0">งานอาชีวศึกษาระบบทวิภาคี</p>
+                                    </td>
+                                    <td class="align-middle text-center text-sm">
+                                        <span class="badge badge-sm badge-secondary">Offline</span>
+                                    </td>
+                                    <td class="align-middle text-center">
+                                        <span class="text-secondary text-xs font-weight-normal">ครูผู้ช่วย</span>
+                                    </td>
+                                    <td class="align-middle">
+                                        <a href="https://rms.trattc.ac.th/pms_graph.php?p=&people_dep_id=327&mod=&title=%A7%D2%B9%CD%D2%AA%D5%C7%C8%D6%A1%C9%D2%C3%D0%BA%BA%B7%C7%D4%C0%D2%A4%D5#data"
+                                            target="_blank" class="text-secondary font-weight-normal text-xs"
+                                            data-toggle="tooltip" data-original-title="Edit user">
+                                            Details
+                                        </a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { toast } from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
+
+toast.success("ยินดีต้อนรับ ฝายวิชาการ", {
+    autoClose: 1500,
+    position: toast.POSITION.TOP_CENTER,
+})
+
+const notify = () => {
+    toast.info("ยินดีต้อนรับ ฝายวิชาการ", {
+        autoClose: 1500,
+    })
+    return notify
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Sarabun&display=swap');
+
+.navbar {
+    font-family: 'Sarabun', sans-serif;
+    font-size: 14px;
+}
+
+.dropdown-item {
+    font-size: 14px;
+}
+
+.card-text {
+    font-family: 'Sarabun', sans-serif;
+    font-size: 14px;
+}
+
+.card-title {
+    font-family: 'Sarabun', sans-serif;
+}
+
+h5 {
+    font-family: 'Sarabun', sans-serif;
+}
+
+h6 {
+    font-family: 'Sarabun', sans-serif;
+}
+
+span {
+    font-family: 'Sarabun', sans-serif;
+}
+
+p {
+    font-family: 'Sarabun', sans-serif;
+    font-size: 14px;
+}
+
+ol {
+    font-family: 'Sarabun', sans-serif;
+    font-size: 16px;
+}
+
+.nav-link {
+    font-family: 'Sarabun', sans-serif;
+    font-size: 14px;
+}</style>
